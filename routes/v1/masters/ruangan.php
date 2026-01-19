bd6d5859806a8f4df8d96654e75e9dc2577a2149
duro06 <andi.meka.025@gmail.com>
2026-01-19 21:37:43 +0700

    anu

--- routes/v1/masters/ruangan.php
+++ routes/v1/masters/ruangan.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
   // 'middleware' => 'auth:api',
-//   'middleware' => 'auth:sanctum',
+  'middleware' => 'auth:sanctum',
   'prefix' => 'master/ruangan'
 ], function () {
   Route::get('/get-list', [RuanganController::class, 'index']);
